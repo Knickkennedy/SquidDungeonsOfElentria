216f0473452992820dd16c30da4adf9210960569
Knickkennedy <knicholas.kennedy@gmail.com>
2018-06-02 13:34:28 +0200

    Added basic inventory system for testing purposes

--- core/src/main/java/roguelike/Components/Equippable.java
+++ core/src/main/java/roguelike/Components/Equippable.java
@@ -1,4 +1,21 @@
 package roguelike.Components;
 
+import org.json.simple.JSONObject;
+import roguelike.Enums.Equipment_Slot;
+
+import java.util.ArrayList;
+import java.util.Set;
+
 public class Equippable extends Component{
+	ArrayList<Equipment_Slot> slots;
+
+	public Equippable(JSONObject object){
+		slots = new ArrayList<>();
+
+		for(Object o : object.keySet()){
+			switch (o.toString()){
+				
+			}
+		}
+	}
 }
